397956a6a01cb65b41bafaea6171af63bc6f0db8
Adhishreegupta <152681307+Adhishreegupta@users.noreply.github.com>
2024-04-11 23:41:52 +0530

    Update remove space in string.c

--- remove space in string.c
+++ remove space in string.c
@@ -3,19 +3,18 @@
 void main()
 {
     char str[40];
-    int i,j,l;
+    int i,j,l,c=0;
     printf("enter string");
     gets(str);
     l=strlen(str);
+    j=0;
     for(i=0;str[i]!='\0';i++)
     {
-        if(str[i]==' '&&str[i+1]!=' ')
+        if(str[i]!=' ')
         {
-            for(j=i;j<l;j++)
-        {
-            str[j]=str[j+1];
-        }
+            str[j++]=str[i];
         }
     }
+    str[j]='\0';
     puts(str);
 }

df382982a445f8738aacdc430803043328094158
Adhishreegupta <152681307+Adhishreegupta@users.noreply.github.com>
2024-04-11 23:07:18 +0530

    Create remove space in string.c

---
+++ remove space in string.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[40];
+    int i,j,l;
+    printf("enter string");
+    gets(str);
+    l=strlen(str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' '&&str[i+1]!=' ')
+        {
+            for(j=i;j<l;j++)
+        {
+            str[j]=str[j+1];
+        }
+        }
+    }
+    str[i]='\0';
+    puts(str);
+}
